191d94e3d4cb29759731ea9cce3dc2e965669c2b
SeungHyunMin <asdf014563@naver.com>
2020-02-17 01:38:13 +0900

    feat : create NewMemoActivity

--- app/src/main/java/com/seunghyun/linememo/ui/list/ListActivity.kt
+++ app/src/main/java/com/seunghyun/linememo/ui/list/ListActivity.kt
@@ -1,14 +1,20 @@
 package com.seunghyun.linememo.ui.list
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.seunghyun.linememo.R
 import com.seunghyun.linememo.databinding.ActivityListBinding
+import com.seunghyun.linememo.ui.new_memo.NewMemoActivity
 
 class ListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        DataBindingUtil.setContentView<ActivityListBinding>(this, R.layout.activity_list)
+        val binding = DataBindingUtil.setContentView<ActivityListBinding>(this, R.layout.activity_list)
+
+        binding.addButton.setOnClickListener {
+            startActivity(Intent(this, NewMemoActivity::class.java))
+        }
     }
 }
---
+++ app/src/main/java/com/seunghyun/linememo/ui/new_memo/NewMemoActivity.kt
@@ -0,0 +1,5 @@
+package com.seunghyun.linememo.ui.new_memo
+
+import androidx.appcompat.app.AppCompatActivity
+
+class NewMemoActivity : AppCompatActivity()
